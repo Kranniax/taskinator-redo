fdb856448dd9228ce8808c3ce3d457306b0e6e1d
Shane Wade <shanebramble96@hotmail.com>
2023-01-12 18:31:38 -0600

    included form validation and form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,24 @@ var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+
+  // check if input values are empty stirngs.
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
+
   // package up data as an object.
   var taskDataObj = {
     name: taskNameInput,
-    type: taskTypeInput
+    type: taskTypeInput,
   };
 
   // send it as an argument to createTaskE1
   createTaskEl(taskDataObj);
 };
-var createTaskEl = function (taskDataObj){
+var createTaskEl = function (taskDataObj) {
   // create  list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
